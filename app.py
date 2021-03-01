73be50ba74f3cb0f7130ef48d831acb1c93d3692
Atul-Bhatt <atulbhatt61@gmail.com>
2021-03-01 22:07:40 +0530

    Redirect and Session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -9,22 +9,38 @@ app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
 
 @app.route('/short-url', methods=['GET', 'POST'])
 def short_url():
-    url = {}
-    url[request.form['code']] = request.form['url']
+    urls = {}
 
     if os.path.exists('urls.txt'):
         with open('urls.txt', mode='r', encoding='utf-8') as open_file:
-            existing_urls = json.load(open_file)
-            if request.form['code'] in existing_urls.keys():
+            urls = json.load(open_file)
+            if request.form['code'] in urls.keys():
                 flash('Short Name Already Exists...')
                 return redirect(url_for('home'))
 
-    with open('urls.txt', mode='a', encoding='utf-8') as file:
-        json.dump(url, file)
+    # if the URL doesn't exist then add it to the file
+    urls[request.form['code']] = request.form['url']
+
+    with open('urls.txt', mode='w', encoding='utf-8') as file:
+        json.dump(urls, file)
+        session[request.form['code']] = True
 
     return render_template('short_url.html', code=request.form['code'], url=request.form['url'])
+
+
+# Redirect the user to the resource
+
+@app.route('/<string:code>')
+def redirect_route(code):
+    if os.path.exists('urls.txt'):
+        with open('urls.txt', mode='r', encoding='utf-8') as file:
+            urls = json.load(file)
+            if code in urls.keys():
+                return redirect(urls[code])
+            else:
+                abort(404)
